459d6e469128d580460b5dfdadb93f0f038642ac
rescher <rick@escher.ca>
2021-03-22 00:58:55 -0400

    converted calculateScore to use streams

--- Play_Mastermind/src/Mastermind.java
+++ Play_Mastermind/src/Mastermind.java
@@ -5,6 +5,7 @@ class Mastermind {
 	int n; //number of possible solutions - n is reduced as we reduce the set of possible solutions
 	
 	int[][] a; //two dimensional array of the digits of all possible solutions
+	int[][] xref = new int[4][12];
 	
 	int g[] = new int[4]; //this is the last guess - object should know its last guess and not have to be told
 	int answer = 0;
@@ -25,10 +26,17 @@ class Mastermind {
 	
 	public void reset() {
 		n = 10 * 10 * 10 * 10; //consider passing this in when the object is created
+		
+		//xref[0] not used
+		//xref[1] not used
+		this.xref[2] = new int[] { 1, 0 };
+		this.xref[3] = new int[] { 1, 0, 0, 3, 3, 1 };
+		this.xref[4] = new int[] { 1, 0, 0, 0, 2, 2, 1, 1, 3, 3, 3, 2 };
+
 		a = new int[n][4];
 		for(int i = 0; i<n; i++)
 			for (int j = 0; j<=3; j++)
-				this.a[i][j] = (i/(int)Math.pow(10,j))%10;		
+				this.a[i][j] = (i/(int)Math.pow(10,j))%10;	
 	}//reset
 	
 	int[] calculateScore(int[] g, int[] a) {
@@ -61,7 +69,9 @@ class Mastermind {
 		return s;
 	}//calculateScore()
 	
-	int calculateScoreStream(int[] g, int[] a) {
+	public int calculateScoreStream2(int[] g, int[] a) {
+		
+		boolean debug = false;
 		
 		//this is the stream version of the method
 		//the method is called from eval()
@@ -71,17 +81,114 @@ class Mastermind {
 		// this code repeatedly compares each digit of the answer to each digit of the guess
 		// the used[] tracks which digits of the answer have already contributed to a count in the score
 		
+		int s[] = {0, 0};
+
+		/*
+		 * 
 		boolean used[] = { false, false, false, false };
-		int s[] = {0, 0};		
+		
 		for (int i = 0; i<=3; i++){
 			if (g[i]==a[i]){
 				s[0]++;
 				used[i]=true;
 			}//if
 		}//for
-		for (int guess=0; guess<=3; guess++) {
+		
+		*/
+	
+		if (debug) System.out.println(Arrays.toString(s));
+		if (debug) System.out.println(Arrays.toString(a));
+		if (debug) System.out.println(Arrays.toString(g));
+		//if (debug) System.out.println(Arrays.toString(used)+"\n");
+		
+		int[] b; //array holding index to elements that do not match
+		b = IntStream.range(0,4).filter(e -> (g[e] != a[e])).toArray();
+		final int m = b.length;
+		s[0] = 4 - m;
+		if (debug) System.out.println("s"+Arrays.toString(s));
+		if (debug) System.out.println("b"+Arrays.toString(b)+"\n");
+		
+		/*
+		s[0]=0;
+		IntStream.range(0,4).filter(e -> (g[e] == a[e])).forEach( e -> {used[e]=true; s[0]++;});		
+		if (debug) System.out.println(Arrays.toString(s));
+		if (debug) System.out.println(Arrays.toString(a));
+		if (debug) System.out.println(Arrays.toString(g));
+		if (debug) System.out.println(Arrays.toString(used));
+		*/
+		
+		int[] x = xref[m];
+		final int c= (m * m) - m;
+		
+		/*
+		 * IntStream    : { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }
+		 * IntStream %4 : { 0, 1, 2, 3, 0, 1, 2, 3, 0, 1,  2,  3 }
+		 * array        : { 1  0  0  0  2  2  1  1  3  3   3   2 }
+		 * 
+		 * ABCD
+		 * abcd
+		 * 0        1        2        3
+		 * A(b,c,d) B(a,c,d) C(a,b,d) D(a,b,c) = 4!/2 = 24/2 = 12
+		 *   1 2 3    0 2 3    0 1 3    0 1 2
+		 */
+		 /*
+		 *
+		 * IntStream    :   { 0, 1, 2, 3, 4, 5 }
+		 * IntStream %3 : a { 0, 1, 2, 0, 1, 2 }
+		 * compare      : x { 1  0  0  2  2  1 }
+		 * 
+		 * ABC
+		 * abc
+		 * 0      1      2
+		 * A(b,c) B(a,c) C(a,b) = 2^3-2 = 6 = 3! = 6
+		 *   1 2    0 2    0 1
+		 */
+		
+		//              0   1   2   3   4   5
+		//    e%3       0   1   2   0   1   2
+		//              6   9   3   6   9   3
+		//              3   5   5   6   6   3  
+		//
+
+		 /*   
+		 *   
+		 * IntStream    : { 0, 1 }
+		 * IntStream %2 : { 0, 1 }
+		 * array        : { 1  0 }
+		 *   
+		 * AB
+		 * ab
+		 * 0    1
+		 * A(b) B(a) = 2^2-2 = 2! = 2
+	 	 *   1    0
+	 	 *   
+		 */ 
+	
+		//IntStream.range(0,c).forEach(e -> System.out.println("a[b[e%m]]"+a[b[e%m]]+", g[x[e]]"+g[x[e]]+", bool="+(a[b[e%m]] == g[x[e]]) ));
+		s[1] = (int) IntStream.range(0,c).filter(e -> a[b[e%m]] == g[x[e]] ).count();
+		if (debug) System.out.println("s: " + Arrays.toString(s));
+		
+		/*
+		int guess;
+		for (int i=0; i<=b.length-1; i++) {
 			// guess iterates over the digits in the guess
-			if (g[guess]!=a[guess]){
+			guess = b[i];
+			IntStream.range(0,b.length-1).filter(e -> (g[e] == a[e])).forEach( e -> {used[e]=true; s[0]++;});
+			
+			for (int j=0; j<=b.length-1; j++) {
+				answer = b[j];
+				if ( (!used[answer]) && (g[guess] == a[answer]) ) {
+					// guess digit matches answer digit but is in the wrong position
+					s[1]++;
+					used[answer] = true;
+					break;
+				}//if
+			}//for answer
+		}// for guess	
+		
+		for (guess=0; guess<=3; guess++) {
+			// guess iterates over the digits in the guess
+			if (g[guess]!=a[guess]){;
 				for (int answer=0; answer<=3; answer++) {
 					if ( (!used[answer]) && (g[guess] == a[answer]) ) {
 						// guess digit matches answer digit but is in the wrong position
@@ -92,8 +199,21 @@ class Mastermind {
 				}//for answer
 			}// if (g[guess]!=a[guess])
 		}// for guess
+		*/
+		
 		return s[0]*10+s[1];
-	}//calculateScoreStream()
+	}//calculateScoreStream2()
+
+	public int calculateScoreStream(int[] g, int[] a) {
+		int s[] = new int[2];
+		final int[] b = IntStream.range(0,4).filter(e -> (g[e] != a[e])).toArray(); //array holding indices to elements that do not match
+		final int m = b.length;
+		s[0] = 4 - m;
+		final int[] x = xref[m];
+		final int c = ( m * m ) - m;
+		s[1] = (int) IntStream.range(0,c).filter(e -> a[b[e%m]] == g[x[e]] ).count();
+		return s[0]*10+s[1];
+	}//calculateScoreStream2()
 
 	void eval(double score) {	
 		if (bStream){
